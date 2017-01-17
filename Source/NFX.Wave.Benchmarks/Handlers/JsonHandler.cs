23bc2605d29d9533981f5469f5efef44fd51dda6
Oleg Ageev <my@cnonim.name>
2017-01-17 21:09:32 +0300

    Enable gcServer and Add PlainText/Json handlers

---
+++ Source/NFX.Wave.Benchmarks/Handlers/JsonHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+using NFX;
+using NFX.Environment;
+using NFX.Serialization.JSON;
+
+namespace NFX.Wave.Benchmarks.Handlers
+{
+  public class JsonHandler : WorkHandler
+  {
+    protected JsonHandler(WorkDispatcher dispatcher, IConfigSectionNode confNode) : base(dispatcher, confNode) { }
+
+    protected override void DoHandleWork(WorkContext work)
+    {
+      work.Response.WriteJSON(new { message = "Hello World!" }, JSONWritingOptions.Compact);
+    }
+  }
+}
---
+++ Source/NFX.Wave.Benchmarks/Handlers/PlainTextHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+using NFX;
+using NFX.Environment;
+
+namespace NFX.Wave.Benchmarks.Handlers
+{
+  public class PlainTextHandler : WorkHandler
+  {
+    private static readonly byte[] CONST_HELLO_WORLD = Encoding.UTF8.GetBytes("Hello, World!");
+
+    protected PlainTextHandler(WorkDispatcher dispatcher, IConfigSectionNode confNode) : base(dispatcher, confNode) { }
+
+    protected override void DoHandleWork(WorkContext work)
+    {
+      var response = work.Response;
+      response.ContentType = Web.ContentType.TEXT;
+
+      response.GetDirectOutputStreamForWriting().Write(CONST_HELLO_WORLD, 0, CONST_HELLO_WORLD.Length);
+    }
+  }
+}
