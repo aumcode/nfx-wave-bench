54025c360078a1aa382be25e8d0c8e9d31d15a6a
Oleg Ageev <my@cnonim.name>
2017-01-17 20:20:10 +0300

    Initial Project Structure
    Added solution, project...
    Added simple MVC Controller

---
+++ Source/NFX.Wave.Benchmarks/Controllers/Index.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using NFX;
+using NFX.Wave.MVC;
+
+namespace NFX.Wave.Benchmarks.Controllers
+{
+  public class Index : Controller
+  {
+    [Action]
+    public object PlainText()
+    {
+      return "Hello World!";
+    }
+
+    [Action]
+    public object Json()
+    {
+      return new { message = "Hello World!" };
+    }
+  }
+}
---
+++ Source/NFX.Wave.Benchmarks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using NFX;
+using NFX.ApplicationModel;
+using NFX.IO;
+
+namespace NFX.Wave.Benchmarks
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        using (var app = new ServiceBaseApplication(args, null))
+        using (var ws = new WaveServer())
+        {
+          ws.Configure(null);
+          ws.Start();
+
+          Console.WriteLine(ws.Name + " started. Press <ENTER> to terminate...");
+          Console.ReadLine();
+        }
+      }
+      catch (Exception error)
+      {
+        ConsoleUtils.Error(error.ToMessageWithType());
+        System.Environment.ExitCode = -1;
+      }
+    }
+  }
+}
